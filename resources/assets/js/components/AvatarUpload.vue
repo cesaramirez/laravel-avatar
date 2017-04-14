e195f70679522a3ca824201b86b787a820f8532c
César Ramírez <cesar.ramirez.sv@gmail.com>
2017-04-13 18:41:25 -0600

    Add vue component

---
+++ resources/assets/js/components/AvatarUpload.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+    <div>
+        <div class="form-group">
+            <label for="avatar">Avatar</label>
+            <input type="file" @change="fileChange">
+            <div class="help-block">
+                Help
+            </div>
+        </div>
+        <div>
+            <input type="hidden" name="avatar_id">
+            <img alt="Current Avatar" class="avatar">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+      fileChange(e) {
+        console.log(e);
+      }
+  }
+}
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,24 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <form action="" method="post">
+                        {{ csrf_field() }}
+                        {{ method_field('PATCH') }}
+                        <div class="form-group">
+                          <avatar-upload></avatar-upload>
+                        </div>
+                        <div class="form-group">
+                          <label for="name">Name</label>
+                          <input type="text"
+                                 class="form-control"
+                                 id="name"
+                                 name="name"
+                                 value="{{ old('name') ?? auth()->user()->name }}">
+                        </div>
+                        <div class="form-group">
+                          <button type="submit" class="btn btn-primary">Save</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
